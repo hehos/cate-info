f661563f834fd32dfcf69a6b12834edc7bcfef60
hehui <304574254@qq.com>
2017-04-09 23:22:01 +0800

    从新恢复项目工作环境

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,5 +1,4 @@
 var path = require('path')
-// var SpritesmithPlugin = require('webpack-spritesmith'); // 处理雪碧图
 var utils = require('./utils')
 var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
@@ -23,7 +22,7 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
+      '@': resolve('src')
     }
   },
   module: {
@@ -31,7 +30,7 @@ module.exports = {
       {
         test: /\.(js|vue)$/,
         loader: 'eslint-loader',
-        enforce: "pre",
+        enforce: 'pre',
         include: [resolve('src'), resolve('test')],
         options: {
           formatter: require('eslint-friendly-formatter')
@@ -50,7 +49,7 @@ module.exports = {
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
-        query: {
+        options: {
           limit: 10000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
@@ -58,56 +57,11 @@ module.exports = {
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
-        query: {
+        options: {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
       }
     ]
-  },
-  plugins: [
-    //  生成雪碧图插件
-    // new SpritesmithPlugin({
-    //   src: {
-    //     cwd: path.resolve(__dirname, '../src/design/myicon'),
-    //     glob: '*.png'
-    //   },
-    //   target: {
-    //     image: path.resolve(__dirname, '../src/image/sprite.png'),
-    //     // css: path.resolve(__dirname, 'src/assets/sprite.css')
-    //     css: [[
-    //       path.resolve(__dirname, '../src/scss/_sprite.scss'),
-    //       { format: 'custom_template' }
-    //     ]]
-    //   },
-    //   apiOptions: {
-    //     cssImageRef: '../image/sprite.png'
-    //   },
-    //   spritesmithOptions: {
-    //     algorithm: 'binary-tree'
-    //   },
-    //   customTemplates: {
-    //     'custom_template': function (data) {
-    //       var shared = [
-    //         '[class^="myicon-"], [class*=" myicon-"] {',
-    //         // '  display: inline-block;',
-    //         // '  vertical-align: middle;',
-    //         '  background-image: url(I);',
-    //         '}'].join("")
-    //         .replace('I', data.sprites[0].image);
-    //
-    //       var perSprite = data.sprites.map(function (sprite) {
-    //         return '.myicon-N { width: Wpx; height: Hpx; background-position: Xpx Ypx; }'
-    //             .replace('N', sprite.name)
-    //             .replace('W', sprite.width)
-    //             .replace('H', sprite.height)
-    //             .replace('X', sprite.offset_x)
-    //             .replace('Y', sprite.offset_y);
-    //       }).join('\n');
-    //
-    //       return shared + '\n' + perSprite;
-    //     }
-    //   }
-    // }),
-  ],
+  }
 }
